37c7bbd0f557c8cf9fe34a18b94b9b6578628078
Bryan C Guner <bryan.guner@gmail.com>
2021-10-22 04:55:19 +0000

    hi

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -1,25 +1,25 @@
 // First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
+import createSchema from "part:@sanity/base/schema-creator";
 
 // Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
+import schemaTypes from "all:part:@sanity/base/schema-type";
 
 // Import user defined schema types
-import config from './config.js';
-import header from './header.js';
-import footer from './footer.js';
-import home from './home.js';
-import contact from './contact.js';
-import page from './page.js';
-import post from './post.js';
-import action from './action.js';
-import form_field from './form_field.js';
-import stackbit_page_meta from './stackbit_page_meta.js';
+import config from "./config.js";
+import header from "./header.js";
+import footer from "./footer.js";
+import home from "./home.js";
+import contact from "./contact.js";
+import page from "./page.js";
+import post from "./post.js";
+import action from "./action.js";
+import form_field from "./form_field.js";
+import stackbit_page_meta from "./stackbit_page_meta.js";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
-  name: 'default',
+  name: "default",
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
@@ -33,6 +33,6 @@ export default createSchema({
     post,
     action,
     form_field,
-    stackbit_page_meta
-    ])
-})
+    stackbit_page_meta,
+  ]),
+});
